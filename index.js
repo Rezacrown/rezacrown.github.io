477c268f2d368f232a09aede294edfc006adb6b8
Rezacrown <aryhidayatfebriana@gmail.com>
2023-01-10 01:22:49 -0800

    perbaikan nav

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-let hamburger = document.getElementById('hamburger');
+let hamburger = document.querySelector('#hamburger');
 let navmobile = document.getElementById('navmobile');
 
 
 
-hamburger, addEventListener('click', function () {
+hamburger. addEventListener('click', function() {
 
-    navmobile.classList.toggle('translate-x-[100vw]')
-    navmobile.classList.toggle('opacity-100')
+        navmobile.classList.toggle('translate-x-[100vw]')
+        navmobile.classList.toggle('opacity-100')
+
 })
+
+
+// window.onscroll = function() {
+
+//     const section1 = document.getElementsByTagName('section')[0]
+
+//     const sectionTop1 = section1.offsetTop
+
+//     console.log(sectionTop1.value)
+
+    
+// }
+
+
+
+
+
 
 
 
